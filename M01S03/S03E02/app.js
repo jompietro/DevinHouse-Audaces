4f0c83b811f99b1ffdf4cbd782383c0ea369e0e2
jompietro <jompietro@gmail.com>
2022-12-17 21:42:52 -0300

    SEMANA 03

---
+++ M01S03/S03E02/app.js
@@ -0,0 +1,23 @@
+console.log('e02-s03-m1');
+
+const entradaNome = document.getElementById('nome');
+const botaoAcao = document.getElementById('acao');
+const pMensagem = document.getElementById('mensagem');
+
+// Resolução:
+/*
+const mensagemOla = (nome) => {
+  return `Olá, ${nome}!`;
+};
+*/
+
+// podemos omitir as chaves e return statement
+const mensagemOla = (nome) => `Olá, ${nome}!`;
+
+const resultado = mensagemOla('Mariana');
+console.log(resultado);
+// "Olá, Mariana!"
+
+botaoAcao.addEventListener('click', () => {
+  pMensagem.innerHTML = mensagemOla(entradaNome.value);
+});
